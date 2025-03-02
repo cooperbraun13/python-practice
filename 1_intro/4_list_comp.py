0940a40299db2e1aa3774ae818ce5d5202e69424
cooperbraun13 <cooperjbraun13@gmail.com>
2025-03-02 14:25:57 -0800

    finished list comp intro

--- 1_intro/4_list_comp.py
+++ 1_intro/4_list_comp.py
@@ -2,16 +2,17 @@ healthy = ["kale chips", "broccoli"]
 backpack = ["pizza", "frozen custard", "apple crisp", "kale chips"]
 
 # print(id(backpack))
-# backpack[:] = [item for item in backpack if item in healthy]
+backpack[:] = [item for item in backpack if item in healthy]
 # slice [:] keeps the same object id
 
+# print(id(backpack))
+print(backpack)
+
+# does same thing, just creates new variable
 healthy_backpack = []
 
 for item in backpack:
     if item in healthy:
-        healthy_backpack.append(item)
+        healthy_backpack.append(item.upper())
 
 print(healthy_backpack)
-
-# print(id(backpack))
-print(backpack)
